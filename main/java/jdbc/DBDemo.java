2b849f6060b8be566b801c73b022ef4b7ab6d48c
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-14 19:10:40 +0530

    Added code: UC2

--- main/java/jdbc/DBDemo.java
+++ main/java/jdbc/DBDemo.java
@@ -1,6 +1,10 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 
 public class DBDemo {
     public static void main(String[] args){
@@ -8,8 +12,26 @@ public class DBDemo {
         String username = "root";
         String password = "@Gunnu123*";
         Connection connection;
-//        try {
-////            Class.forName()
-//        }
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        listDrivers();
+        System.out.println("Connecting to database "+jdbcURL);
+        try {
+            connection = DriverManager.getConnection(jdbcURL,username,password);
+            System.out.println("Connection is successful !!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driver = DriverManager.getDrivers();
+        while (driver.hasMoreElements()){
+            Driver driverClass = (Driver) driver.nextElement();
+            System.out.println(driverClass);
+        }
     }
 }
