3f4c2afe40c99e9a9e97010bf6936714c3a1ee19
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-14 19:47:33 +0530

    Refactored Code

--- main/java/jdbc/entity/EmployeeData.java
+++ main/java/jdbc/entity/EmployeeData.java
@@ -4,8 +4,15 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 public class EmployeeData {
+    public int id;
+    public String name;
+    public int salary;
+    public Date startDate;
 
     public EmployeeData(int id, String name, int salary, Date startDate) {
-
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.startDate = startDate;
     }
 }
